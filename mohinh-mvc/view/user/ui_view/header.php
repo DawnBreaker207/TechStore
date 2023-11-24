b3032ab9ce6fe96c4a444023ec428b88889accd1
Ngo Tung Anh <tunganhngo207@gmail.com>
2023-11-24 22:39:09 +0700

    UI: Fix UI and CSS

--- mohinh-mvc/view/user/ui_view/header.php
+++ mohinh-mvc/view/user/ui_view/header.php
@@ -9,6 +9,10 @@
     <link rel="stylesheet" href="accset/css/menu.css" />
     <link rel="stylesheet" href="accset/css/list-product.css">
     <link rel="stylesheet" href="accset/css/detail.css">
+    <link rel="stylesheet" href="accset/css/common.css">
+    <link rel="stylesheet" href="accset/css/header.css">
+    <link rel="stylesheet" href="accset/css/footer.css">
+    <link rel="stylesheet" href="accset/css/sign_up,log_in.css">
     <!--End Link CSS-->
     <!--    Normalize CSS-->
     <link rel="stylesheet" href="https://necolas.github.io/normalize.css/8.0.1/normalize.css">
@@ -16,46 +20,89 @@
     <!--Inter Font-->
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link
-        href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500&family=Noto+Sans:wght@500&family=Roboto&display=swap"
-        rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500&family=Noto+Sans:wght@500&family=Roboto&display=swap" rel="stylesheet" />
     <!--End Inter Font-->
     <!--Poppins Font-->
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link
-        href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500&family=Noto+Sans:wght@500&family=Poppins&family=Roboto&display=swap"
-        rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500&family=Noto+Sans:wght@500&family=Poppins&family=Roboto&display=swap" rel="stylesheet" />
     <!--End Poppins Font-->
 </head>
 
 <body>
-<section class="header wrapper">
+    <section class="header wrapper">
         <div class="header-row">
             <div class="header-col">
                 <a href="">
-                    <img src="image/header/logo_header.svg" alt="" class="nav__img" />
+                    <img src="accset/icon/header/logo_header.svg" alt="" class="nav__img" />
                 </a>
             </div>
             <div class="header-col">
                 <ul class="header-list">
-                    <li><a href="">Home</a></li>
-                    <li><a href="">Contact</a></li>
-                    <li><a href="">About</a></li>
-                    <li><a href="index.php?act=sigin">Sign Up</a></li>
+                    <li><a href="" class="header__link">Home</a></li>
+                    <li><a href="" class="header__link">Contact</a></li>
+                    <li><a href="" class="header__link">About</a></li>
+                    <li><a href="view/login.php" class="header__link">Sign Up</a></li>
                 </ul>
             </div>
             <div class="header-col">
                 <div class="header-function">
                     <div class="header-search">
                         <input type="text" name="" id="" placeholder="What are you looking for?" />
-                        <img src="image/icon/search.svg" alt="" class="cart__img" />
+                        <img src="accset/icon/search.svg" alt="" class="cart__img" />
                     </div>
-                    <div class="header-cart">
-                        <a href="">
-                            <img src="image/icon/cart.svg" alt="" srcset="">
-                        </a>
+                    <div class="header-nav">
+                        <div class="header-cart">
+                            <a href="" class="header-cart__link">
+                                <img src="accset/icon/cart.svg" alt="" class="cart__img " />
+                                <span class="header-cart--notifi">2</span>
+                            </a>
+                        </div>
+                        <div class="header-user">
+                            <div class="user-block">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="transparent" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="icon-l user__account">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
+                                </svg>
+
+
+                                <div class="user-option">
+                                    <div class="option-item">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="icon-lm">
+                                            <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
+                                        </svg>
+                                        <p>Manage My Account</p>
+                                    </div>
+                                    <div class="option-item">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="icon-lm">
+                                            <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
+                                        </svg>
+                                        <p>Manage My Account</p>
+                                    </div>
+                                    <div class="option-item">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="icon-lm">
+                                            <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
+                                        </svg>
+                                        <p>Manage My Account</p>
+                                    </div>
+                                    <div class="option-item">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="icon-lm">
+                                            <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
+                                        </svg>
+                                        <p>Manage My Account</p>
+                                    </div>
+                                    <div class="option-item">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="icon-lm">
+                                            <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
+                                        </svg>
+                                        <p>Manage My Account</p>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
                     </div>
+
+
                 </div>
             </div>
         </div>
