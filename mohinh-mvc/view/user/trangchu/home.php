86ca51322ed667755241c8aac381e6b351625f99
DawnBreaker207 <tunganhngo207@gmail.com>
2023-12-04 17:42:54 +0700

    no message

--- mohinh-mvc/view/user/trangchu/home.php
+++ mohinh-mvc/view/user/trangchu/home.php
@@ -4,46 +4,45 @@
         <section class="category">
 
 
-            <a href="index.php?act=product&nd=maDm&ma_dm=4">CPU<img src="accset/icon/dropdown-black.svg" alt=""/></a>
+            <a href="index.php?act=product&nd=maDm&ma_dm=4">CPU<img src="accset/icon/dropdown-black.svg" alt="" /></a>
 
 
-            <a href="index.php?act=product&nd=maDm&ma_dm=3">RAM<img src="accset/icon/dropdown-black.svg" alt=""/></a>
+            <a href="index.php?act=product&nd=maDm&ma_dm=3">RAM<img src="accset/icon/dropdown-black.svg" alt="" /></a>
 
 
-            <a href="index.php?act=product&nd=maDm&ma_dm=6">SSD<img src="accset/icon/dropdown-black.svg" alt=""/></a>
+            <a href="index.php?act=product&nd=maDm&ma_dm=6">SSD<img src="accset/icon/dropdown-black.svg" alt="" /></a>
 
 
-            <a href="index.php?act=product&nd=maDm&ma_dm=7">VGA<img src="accset/icon/dropdown-black.svg" alt=""/></a>
+            <a href="index.php?act=product&nd=maDm&ma_dm=7">VGA<img src="accset/icon/dropdown-black.svg" alt="" /></a>
 
 
-            <a href="index.php?act=product&nd=maDm&ma_dm=5">HDD<img src="accset/icon/dropdown-black.svg" alt=""/></a>
+            <a href="index.php?act=product&nd=maDm&ma_dm=5">HDD<img src="accset/icon/dropdown-black.svg" alt="" /></a>
 
 
-            <a href="">Memory<img src="accset/icon/dropdown-black.svg" alt=""/></a>
+            <a href="">Memory<img src="accset/icon/dropdown-black.svg" alt="" /></a>
 
 
-            <a href="">Display<img src="accset/icon/dropdown-black.svg" alt=""/></a>
+            <a href="">Display<img src="accset/icon/dropdown-black.svg" alt="" /></a>
 
 
-            <a href="">Power<img src="accset/icon/dropdown-black.svg" alt=""/></a>
+            <a href="">Power<img src="accset/icon/dropdown-black.svg" alt="" /></a>
 
 
-            <a href="">Others<img src="accset/icon/dropdown-black.svg" alt=""/></a>
+            <a href="">Others<img src="accset/icon/dropdown-black.svg" alt="" /></a>
 
 
         </section>
         <!-- End .category -->
-        <hr class="hero-column"/>
+        <hr class="hero-column" />
         <div class="hero-slider">
-            <div class="hero-slide active-slide"
-                 style="background-image: url(accset/img/slide/slide_0.png); width: 100%">
+            <div class="hero-slide active-slide" style="background-image: url(accset/img/slide/slide_0.png); width: 100%">
                 <div class="slide-title">
-                    <img src="image/slide/logo.png" alt=""/>
+                    <img src="image/slide/logo.png" alt="" />
                     <h1 class="hero__title">
                         Unlock Peak Performance Only With AMD
                         <span class="hero-link">
                             <a href="" class="product__link">Shop now</a>
-                            <img src="accset/icon/arrow-right-white.svg" alt=""/>
+                            <img src="accset/icon/arrow-right-white.svg" alt="" />
                         </span>
                     </h1>
                 </div>
@@ -80,41 +79,29 @@
                 <h2 class="section__title">Browse By Branding</h2>
                 <div class="section-nav">
                     <div class="button-nav" id="slide-pre">
-                        <img
-                                src="accset/icon/arrow-left-black.svg"
-                                alt=""
-                                class="button__icon"
-                        />
+                        <img src="accset/icon/arrow-left-black.svg" alt="" class="button__icon" />
                     </div>
                     <div class="button-nav" id="slide-next">
-                        <img
-                                src="accset/icon/arrow-right-black.svg"
-                                alt=""
-                                class="button__icon"
-                        />
+                        <img src="accset/icon/arrow-right-black.svg" alt="" class="button__icon" />
                     </div>
                 </div>
             </div>
         </header>
         <div class="section-body">
             <div class="section-body">
-                <div class="brand-row snap-inline"> 
-                    <?php foreach($loadAllNSX as $nsx){
-                                ?>
-                    <div class="brand-col">
-                        <div class="brand-logo">
-                           
-                            <a href="index.php?act=product&nd=nsx&ma_nsx=3">
-                                <img
-                                        src="<?php echo $nsx['img_nsx']; ?>"
-                                        alt=""
-                                        class="brand__img" width="150px" height="150px"
-                                />
-                            </a>
-                           
+                <div class="brand-row snap-inline">
+                    <?php foreach ($loadAllNSX as $nsx) {
+                    ?>
+                        <div class="brand-col">
+                            <div class="brand-logo">
+
+                                <a href="index.php?act=product&nd=nsx&ma_nsx=3">
+                                    <img src="<?php echo $nsx['img']; ?>" alt="" class="brand__img" width="150px" height="150px" />
+                                </a>
+
+                            </div>
+                            <!--End .brand-logo-->
                         </div>
-                        <!--End .brand-logo-->
-                    </div> 
                     <?php  }  ?>
                     <!--End .brand-col-->
 
@@ -145,37 +132,25 @@
 
                 <?php foreach ($top_seller as $sp) {
                     extract($sp);
-                    ?>
+                ?>
                     <!-- form thông tin sp -->
-                   
+
                     <div class="section-col">
 
                         <div class="product">
                             <div class="product-header">
                                 <div class="product-image">
                                     <a href="index.php?act=ctsp&ma_sp=<?php echo $ma_sp; ?>">
-                                        <img
-                                                src="<?php echo $img; ?>"
-                                                style="height: 250px; "
-                                                alt=""
-                                                class="product__img"
-                                        />
+                                        <img src="<?php echo $img; ?>" style="height: 250px; " alt="" class="product__img" />
                                     </a>
-                                    <form action="index.php?act=addToCart" method="post" enctype="multipart/form-data" >
-                                    <input type="hidden" name="masp" value="<?php echo $ma_sp; ?>">
-                                    <input type="hidden" name="tensp" value="<?php echo $ten_sp; ?>">
-                                    <input type="hidden" name="img" value="<?php echo $img;?>">
-                                    <input type="hidden" name="soluong" value="<?php echo $soluong; ?>">
-                                    <input type="hidden" name="gia" value="<?php echo $gia; ?>">
-                                    <input class="product-cart"  type="submit" value="Add to cart">
-                                    </form>
+                                    <a href="" class="product-cart">Add to cart</a>
                                 </div>
                                 <div class="product-tab">
                                     <a class="product-item" href="#">
-                                        <img src="accset/icon/heart.svg" alt=""/>
+                                        <img src="accset/icon/heart.svg" alt="" />
                                     </a>
                                     <a class="product-item" href="#">
-                                        <img src="accset/icon/view.svg" alt=""/>
+                                        <img src="accset/icon/view.svg" alt="" />
                                     </a>
                                 </div>
                             </div>
@@ -189,13 +164,13 @@
                                     <?php echo $gia; ?> VND<span class="product__sale">$1160</span>
                                 </div>
                                 <span class="product__rate">
-                    <img src="accset/icon/star.svg" alt=""/>
-                    <img src="accset/icon/star.svg" alt=""/>
-                    <img src="accset/icon/star.svg" alt=""/>
-                    <img src="accset/icon/star.svg" alt=""/>
-                    <img src="accset/icon/half-star.svg" alt=""/>
-                    <span class="product__total">(65)</span>
-                  </span>
+                                    <img src="accset/icon/star.svg" alt="" />
+                                    <img src="accset/icon/star.svg" alt="" />
+                                    <img src="accset/icon/star.svg" alt="" />
+                                    <img src="accset/icon/star.svg" alt="" />
+                                    <img src="accset/icon/half-star.svg" alt="" />
+                                    <span class="product__total">(65)</span>
+                                </span>
                             </div>
                         </div>
 
@@ -266,57 +241,52 @@
 
                 <?php foreach ($product as $sp) {
                     extract($sp);
-                    ?>
-                    
+                ?>
+
                     <div class="section-col">
 
                         <div class="product">
                             <div class="product-header">
                                 <div class="product-image">
                                     <a href="index.php?act=ctsp&nd=ctsp&ma_sp=<?php echo $ma_sp; ?>">
-                                        <img
-                                                src="<?php echo $img; ?>"
-                                                style="height: 250px; "
-                                                alt=""
-                                                class="product__img"
-                                        />
+                                        <img src="<?php echo $img; ?>" style="height: 250px; " alt="" class="product__img" />
                                     </a>
-                                    <form action="index.php?act=addToCart" method="post" enctype="multipart/form-data" >
-                                    <input type="hidden" name="masp" value="<?php echo $ma_sp; ?>">
-                                    <input type="hidden" name="tensp" value="<?php echo $ten_sp; ?>">
-                                    <input type="hidden" name="img" value="<?php echo $img;?>">
-                                    <input type="hidden" name="soluong" value="<?php echo $soluong; ?>">
-                                    <input type="hidden" name="gia" value="<?php echo $gia; ?>">
-                                    <input class="product-cart"  type="submit" value="Add to cart">
+                                    <form action="index.php?act=addToCart" method="post" enctype="multipart/form-data">
+                                        <input type="hidden" name="masp" value="<?php echo $ma_sp; ?>">
+                                        <input type="hidden" name="tensp" value="<?php echo $ten_sp; ?>">
+                                        <input type="hidden" name="img" value="<?php echo $img; ?>">
+                                        <input type="hidden" name="soluong" value="<?php echo $soluong; ?>">
+                                        <input type="hidden" name="gia" value="<?php echo $gia; ?>">
+                                        <input class="product-cart" type="submit" value="Add to cart">
                                     </form>
                                 </div>
-                                
+
                                 <div class="product-tab">
                                     <a class="product-item" href="#">
-                                        <img src="accset/icon/heart.svg" alt=""/>
+                                        <img src="accset/icon/heart.svg" alt="" />
                                     </a>
                                     <a class="product-item" href="#">
-                                        <img src="accset/icon/view.svg" alt=""/>
+                                        <img src="accset/icon/view.svg" alt="" />
                                     </a>
                                 </div>
                             </div>
                             <div class="product-body">
                                 <a href="">
                                     <h3 href="" class="product__title">
-                                        <?php echo $ten_sp;?>
+                                        <?php echo $ten_sp; ?>
                                     </h3>
                                 </a>
                                 <div class="product__price">
                                     <?php echo $gia; ?> VND<span class="product__sale">$1160</span>
                                 </div>
                                 <span class="product__rate">
-                    <img src="accset/icon/star.svg" alt=""/>
-                    <img src="accset/icon/star.svg" alt=""/>
-                    <img src="accset/icon/star.svg" alt=""/>
-                    <img src="accset/icon/star.svg" alt=""/>
-                    <img src="accset/icon/half-star.svg" alt=""/>
-                    <span class="product__total">(65)</span>
-                  </span>
+                                    <img src="accset/icon/star.svg" alt="" />
+                                    <img src="accset/icon/star.svg" alt="" />
+                                    <img src="accset/icon/star.svg" alt="" />
+                                    <img src="accset/icon/star.svg" alt="" />
+                                    <img src="accset/icon/half-star.svg" alt="" />
+                                    <span class="product__total">(65)</span>
+                                </span>
                             </div>
                         </div>
 
@@ -405,7 +375,7 @@
         <div class="service-row">
             <div class="service-col">
                 <div class="service-image-wrapper">
-                    <img src="accset/icon/service/service_logo.svg" alt="" class="service__image"/>
+                    <img src="accset/icon/service/service_logo.svg" alt="" class="service__image" />
                 </div>
                 <div class="service-title">
                     <h3 class="service__title">FREE AND FAST DELIVERY</h3>
@@ -417,7 +387,7 @@
             <!--End .service-col-->
             <div class="service-col">
                 <div class="service-image-wrapper">
-                    <img src="accset/icon/service/service_logo_1.svg" alt="" class="service__image"/>
+                    <img src="accset/icon/service/service_logo_1.svg" alt="" class="service__image" />
                 </div>
                 <div class="service-title">
                     <h3 class="service__title">24/7 CUSTOMER SERVICE</h3>
@@ -427,7 +397,7 @@
             <!--End .service-col-->
             <div class="service-col">
                 <div class="service-image-wrapper">
-                    <img src="accset/icon/service/service_logo_2.svg" alt="" class="service__image"/>
+                    <img src="accset/icon/service/service_logo_2.svg" alt="" class="service__image" />
                 </div>
                 <div class="service-title">
                     <h3 class="service__title">MONEY BACK GUARANTEE</h3>
